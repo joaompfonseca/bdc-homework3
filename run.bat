79bf5c1fc41b4af56f6e057ea04a548cb2ce0944
João Fonseca <joao.fonseca@ua.pt>
2025-06-07 16:59:05 +0200

    add bat scripts for powershell

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+if not exist runs (
+    mkdir runs
+)
+
+for /l %%i in (1,1,3) do (
+    python G11HW3.py 8886 100000 9 100 10 > runs\8886_100000_9_100_10_run%%i.txt
+    python G11HW3.py 8886 100000 9 100 20 > runs\8886_100000_9_100_20_run%%i.txt
+    python G11HW3.py 8886 100000 9 100 50 > runs\8886_100000_9_100_50_run%%i.txt
+    python G11HW3.py 8886 100000 9 100 100 > runs\8886_100000_9_100_100_run%%i.txt
+
+    python G11HW3.py 8886 100000 9 5 10 > runs\8886_100000_9_5_10_run%%i.txt
+    python G11HW3.py 8886 100000 9 10 10 > runs\8886_100000_9_10_10_run%%i.txt
+    python G11HW3.py 8886 100000 9 20 10 > runs\8886_100000_9_20_10_run%%i.txt
+    python G11HW3.py 8886 100000 9 50 10 > runs\8886_100000_9_50_10_run%%i.txt
+)
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+if not exist runs (
+    mkdir runs
+)
+
+python G11HW3.py 8886 200000 7 100 10 > runs\8886_200000_7_100_10_test.txt
+python G11HW3.py 8886 200000 7 100 20 > runs\8886_200000_7_100_20_test.txt
