4238e5216c741c1bbd16b60cfd00cd954de6b6f5
João Fonseca <joao.fonseca@ua.pt>
2025-06-07 16:04:57 +0200

    update scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-#                port T      D W   K
-python G11HW3.py 8886 100000 9 100  10
-python G11HW3.py 8886 100000 9 100  20
-python G11HW3.py 8886 100000 9 100  50
-python G11HW3.py 8886 100000 9 100 100
+mkdir -p runs
 
-python G11HW3.py 8886 100000 9   5  10
-python G11HW3.py 8886 100000 9  10  10
-python G11HW3.py 8886 100000 9  20  10
-python G11HW3.py 8886 100000 9  50  10
+for i in {1,2,3}; do
+  python G11HW3.py 8886 100000 9 100 10 >runs/8886_100000_9_100_10_run${i}.txt
+  python G11HW3.py 8886 100000 9 100 20 >runs/8886_100000_9_100_20_run${i}.txt
+  python G11HW3.py 8886 100000 9 100 50 >runs/8886_100000_9_100_50_run${i}.txt
+  python G11HW3.py 8886 100000 9 100 100 >runs/8886_100000_9_100_100_run${i}.txt
+
+  python G11HW3.py 8886 100000 9 5 10 >runs/8886_100000_9_5_10_run${i}.txt
+  python G11HW3.py 8886 100000 9 10 10 >runs/8886_100000_9_10_10_run${i}.txt
+  python G11HW3.py 8886 100000 9 20 10 >runs/8886_100000_9_20_10_run${i}.txt
+  python G11HW3.py 8886 100000 9 50 10 >runs/8886_100000_9_50_10_run${i}.txt
+done
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-python G11HW3.py 8886 200000 7 100 10 > runs/test_8886_200000_7_100_10.txt
-python G11HW3.py 8886 200000 7 100 20 > runs/test_8886_200000_7_100_20.txt
+mkdir -p runs
+
+python G11HW3.py 8886 200000 7 100 10 >runs/8886_200000_7_100_10_test.txt
+python G11HW3.py 8886 200000 7 100 20 >runs/8886_200000_7_100_20_test.txt
